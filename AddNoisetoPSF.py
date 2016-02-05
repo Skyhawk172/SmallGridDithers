afcc42fe5788dd93c736287a5abc2f56e3bb18cd
Charles-Philippe Lajoie <lajoiecp@gmail.com>
2016-02-05 14:47:25 -0500

    Review of whole script: cleaned up the main function; more to come

--- AddNoisetoPSF.py
+++ AddNoisetoPSF.py
@@ -2,7 +2,7 @@
 #
 # WHAT: Program that takes normalized coronagraphic PSF and 
 # scales them up using pysynphot according to specified
-# Teff, logg, z, and Vabs.
+# Teff, logg, z.
 #
 # The model is based on the JWST Simulator (see Brian
 # York) and uses the same unmodified modules.
@@ -40,7 +40,6 @@ MY_BGLEVEL={
 
 
 class Spectrum(object):
-
     def __init__(self, instr, filt):
         self.instr= instr
         self.filt = filt
@@ -61,12 +60,14 @@ class Spectrum(object):
             print ' Using default Pysynphot Vega spectrum'
         else: 
             print self.getFiltername(), self.getSpectrumInfo()
+            self.star= S.Icat('phoenix',self.T,self.z,self.g) #FLAM surface flux units, i.e. ergs cm-2 s-1 A-1
+
             #star  = S.BlackBody(6000)
             #star= S.Icat('phoenix',5800,0.,4.4) #FLAM surface flux units, i.e. ergs cm-2 s-1 A-1
-            self.star= S.Icat('phoenix',self.T,self.z,self.g) #FLAM surface flux units, i.e. ergs cm-2 s-1 A-1
             #self.star= self.star.renorm(self.j_v_abs,'vegamag',self.johnson_v) #renorm spectrum to (abs_mag, units system, band)
         
         return self.star
+
 
     def getBandpass(self): 
         if self.instr=='MIRI':
@@ -105,7 +106,6 @@ class Spectrum(object):
                 filters=glob.glob('/Users/lajoie/Documents/Work/Projects/JWST/Simulations/Coronagraphs/'+self.instr+'/filters/F*.dat')
                 print [filters[i].split('/')[-1][:-4] for i in xrange(len(filters))],"\n"
                 sys.exit()
-
 
             self.filt_wave = filt_file[:,0]*1e4 #microns in file, so convert to angstroms
             self.filt_thru = filt_file[:,1]
@@ -186,8 +186,34 @@ class Spectrum(object):
 
 
 
+def AddBKGRDPoissonNoise(data,Bkgrd,absVal=False):
+        """
+        Generate Poisson noise and add it to the internal image.
+
+        SEE BRIAN YORK'S JwstImage.py routine
+
+        Parameters
+        ----------
+        absVal: bool, optional
+            Take absolute value of `noiseData`.
+        """
+
+        # 1. Gaussian distribution - shot noise.
+        # 2. Poisson = Gaussian * SQRT(image)
+        noiseData = np.random.normal(size=data.shape) * np.sqrt(Bkgrd)
+
+        # Avoid negative noise
+        if absVal: noiseData = np.abs(noiseData)
+        
+        data += noiseData
+        del noiseData
+        return data
+
+
+
 def convert(in_path,out_path,instr,filt,Teff=5800.,z=0.0,logg=4.44,radius=1.,distance=10.,vega=False,exptime=1.0,clobber=False,nonoise=False,run=1,**kwargs):
-    if instr=='NIRCam': filt=filt[:5] #strip the mask string off
+
+    if instr=='NIRCam': filt=filt[:5] # Strip the mask string off, e.g. F210M-MASK210R
     spec=Spectrum(instr,filt)
 
     if instr=='MIRI': 
@@ -206,16 +232,15 @@ def convert(in_path,out_path,instr,filt,Teff=5800.,z=0.0,logg=4.44,radius=1.,dis
             xdim,ydim = (222,222)
             nircamMode = 'short'
 
-    bandpass=spec.getBandpass()
-    # default Sun at 10pc: Teff=5800, R=1 Rsun, D=10 pc, z=0, logg=4.44, vega=False
-    # returns angstroms, flam
-    star=spec.getSpectrum(Teff=Teff,z=z,logg=logg,vega=vega) 
+    bandpass=spec.getBandpass()  
+    star=spec.getSpectrum(Teff=Teff,z=z,logg=logg,vega=vega) # returns angstroms, flam
 
     if vega==False: star*=(radius*2.254e-8/distance)**2 #distance in units of parsec
 
     star.convert("photlam") #photons s^-1 cm^-2 A^-1
     obs=S.Observation(star,bandpass) 
-    print 'units: ',star.waveunits, star.fluxunits
+    #print 'units: ',star.waveunits, star.fluxunits
+
 
 #####################################################################################
 #   BELOW: NOT NECESSARY SINCE THIS IS TAKEN INTO ACCOUNT WHEN GENERATING THE PSFs
@@ -229,10 +254,8 @@ def convert(in_path,out_path,instr,filt,Teff=5800.,z=0.0,logg=4.44,radius=1.,dis
 #        bp_OTE  = spec.getMIRI_OTE_transmission()
 #        obs     = S.Observation(obs,bp_OTE)
 
-
-    ncounts=np.trapz(obs.flux,obs.wave)*DefaultSettings.JWSTREFS['area'] #multiply by collecting area!!!
+    ncounts=np.trapz(obs.flux,obs.wave)*DefaultSettings.JWSTREFS['area']  #multiply by collecting area
     print "# phot s^-1 :", ncounts
-
 
     runNumber=run
     string='run'+str(runNumber)+'_'
@@ -250,9 +273,7 @@ def convert(in_path,out_path,instr,filt,Teff=5800.,z=0.0,logg=4.44,radius=1.,dis
             if ic==0: print '  --> Filter '+img.filterName+" not in DefaultSettings.py for background levels. Using Lajoie's numbers.\n"
             background = MY_BGLEVEL[img.instrument][img.filterName]
 
-
         img.exptime=exptime #that actually multiples the image by the exposure time?!?
-        
 
         if nonoise==False:
             print '   --> Adding noise to PSF'
@@ -280,8 +301,6 @@ def convert(in_path,out_path,instr,filt,Teff=5800.,z=0.0,logg=4.44,radius=1.,dis
         else: 
             img.__imul__( ncounts )
 
-
-
     #   DETECTOR SATURATION???
     #    img._data[img._data>SATUR_LVL[img.instrument]]=SATUR_LVL[img.instrument]
 
@@ -298,128 +317,100 @@ def convert(in_path,out_path,instr,filt,Teff=5800.,z=0.0,logg=4.44,radius=1.,dis
 
 
 
-def AddBKGRDPoissonNoise(data,Bkgrd,absVal=False):
-        """
-        Generate Poisson noise and add it to the internal image.
-
-        SEE BRIAN YORK'S JwstImage.py routine
-
-        Parameters
-        ----------
-        absVal: bool, optional
-            Take absolute value of `noiseData`.
-        """
-
-        # 1. Gaussian distribution - shot noise.
-        # 2. Poisson = Gaussian * SQRT(image)
-        noiseData = np.random.normal(size=data.shape) * np.sqrt(Bkgrd)
-
-        # Avoid negative noise
-        if absVal: noiseData = np.abs(noiseData)
-        
-        data += noiseData
-        del noiseData
-        return data
-
-
-#########################################
-# MAIN PROGRAM:
-#########################################
-
-
-##############################
-# PARSE COMMAND LINE ARGUMENTS
-##############################
-parser = argparse.ArgumentParser()
-
-parser.add_argument("-I"       , metavar="Instrument",required=True, help="JWST instrument")
-parser.add_argument("-f"       , metavar="FILTER",required=True, help="Filter to use")
-parser.add_argument("-step"    , required=True,       default=20, help="Dither grid step size (default: 20 mas)")
-parser.add_argument("-jitter"  , required=True,       default=0 , help="sigma Jitter (default: 0 mas)")
-parser.add_argument("--t"      , type=float, default=5800, help="effective temperature (default: solar T_eff=5800 K)")
-parser.add_argument("--z"      , type=float, default=0.0 , help="metallicity (default: solar z=0)")
-parser.add_argument("--g"      , type=float, default=4.44, help="surface gravity (default: solar log_g=4.44)")
-
-parser.add_argument("--R"      , type=float, default=1 , help="radius of star (default: 1 Rsun )")
-parser.add_argument("--dist"   , type=float, default=10, help="distance to star parsec (default: 10 pc)")
-
-#parser.add_argument("--Vabs"   , type=float, default=4.83, help="scale to Johnson V abs. (default: solar log_g=4.83)")
-parser.add_argument("--exptime", type=float, default=1.0 , help="exposure time (default: 1 sec)")
-parser.add_argument("--run"    ,             default="all"   , help="run number or ''all'' (default: all)")
-parser.add_argument("--vega"   , action="store_true" , default=False, help="use Vega spectrum (default: False)")
-parser.add_argument("--clobber", action="store_true" , default=False, help="overwrite output files (default: False)")
-parser.add_argument("--nonoise", action="store_true", default=False, help="to turn off noise sources (default: add noise)")
-parser.add_argument("--rms"    , type=int, default=400, help="select which OPD rms to use, if available (default: 400 nm)")
-
-
-args = parser.parse_args()
-
-instr=(args.I).upper()
-if instr=='NIRCAM': instr='NIRCam'
-
-filt=(args.f).upper() 
-kwargs={'Teff':args.t,
-        'z':args.z,
-        'logg':args.g,
-        'radius' :args.R,
-        'distance':args.dist,
-        'vega':args.vega,
-        'exptime':args.exptime,
-        'clobber':args.clobber,
-        'nonoise':args.nonoise
-}
-
-##############################
-# SET UP PATHS
-##############################
-path    ="/Users/lajoie/Documents/Work/Projects/JWST/Simulations/Coronagraphs/MIRI/Dither-LOCI/Results/"
-in_path =path+instr+'/'+args.f.upper()+"/DitherStep"+args.step+"/Jitter"+args.jitter+"/Originals"
-out_path=path+instr+'/'+args.f.upper()+"/DitherStep"+args.step+"/Jitter"+args.jitter+"/"
-
-
-if args.rms!=400: in_path+= str(args.rms)+'nm/'#-Grid25pts/'
-else:             in_path+='/'
-
-
-print ' \nInput original PSFs: ',in_path,'\n'
-if os.path.isdir(in_path)==False: 
-    print "   --> Input directory does not exist.\n"
-    sys.exit()
-
-if args.vega==False: out_path+='T'+str(args.t)+'_logg'+str(args.g)+'_z'+str(args.z)+'_R'+str(args.R)+'_D'+str(args.dist)+'_t'+str(args.exptime)
-if args.vega==True : out_path+='Vega'+'_t'+str(args.exptime)
-if args.nonoise==True: out_path+='_NoNoise'
-
-if args.rms!=400: out_path+='_'+str(args.rms)+'nm'
-out_path+='/'
-
-if os.path.exists(out_path): 
-    print '\nOutput folder',out_path[38:],'already exists\n'
-else: 
-    os.mkdir(out_path)
-    print '\nCreating folder',out_path[38:]+'\n'
 
 
 
-##############################
-# CALCULATE NRUNS IN DIRECTORY
-##############################
-infiles=glob.glob(in_path+"*ScienceTarget*")
-print in_path
-splits=list([infiles[i].split("_") for i in xrange(len(infiles))])
-print splits
-nruns=np.max( [int(splits[i][2].strip("run")) for i in xrange(len(splits))] )
+
+#-----------------------------------#
+# MAIN Method
+#-----------------------------------#
+if __name__ == "__main__":
+
+    #-----------------------------------#
+    # PARSE COMMAND LINE ARGUMENTS:
+    #-----------------------------------#
+    parser = argparse.ArgumentParser(description="Add simple noise sources to pre-generated PSFs")
+
+    parser.add_argument("-I"       , metavar="Instrument",required=True, help="JWST instrument")
+    parser.add_argument("-f"       , metavar="FILTER",required=True, help="Filter to use")
+    parser.add_argument("-step"    , required=True, default=20, help="Dither grid step size (default: 20 mas)")
+    parser.add_argument("-jitter"  , required=True, default=0 , help="sigma Jitter (default: 0 mas)")
+    parser.add_argument("--t"      , type=float, default=5800,  help="effective temperature (default: solar T_eff=5800 K)")
+    parser.add_argument("--z"      , type=float, default=0.0 ,  help="metallicity (default: solar z=0)")
+    parser.add_argument("--g"      , type=float, default=4.44,  help="surface gravity (default: solar log_g=4.44)")
+    parser.add_argument("--R"      , type=float, default=1 ,    help="radius of star (default: 1 Rsun )")
+    parser.add_argument("--dist"   , type=float, default=10,    help="distance to star parsec (default: 10 pc)")
+    parser.add_argument("--exptime", type=float, default=1.0 ,  help="exposure time (default: 1 sec)")
+    parser.add_argument("--run"    ,             default="all", help="run number (default: all)")
+    parser.add_argument("--vega"   , action="store_true", default=False, help="use Vega spectrum (default: False)")
+    parser.add_argument("--clobber", action="store_true", default=False, help="overwrite output files (default: False)")
+    parser.add_argument("--nonoise", action="store_true", default=False, help="to turn off noise sources (default: add noise)")
+    parser.add_argument("--rms"    , type=int,            default=400,   help="select which OPD rms to use, if available (default: 400 nm)")
+
+    args = parser.parse_args()
+
+    instr=(args.I).upper()
+    if instr=='NIRCAM': instr='NIRCam'
+    filt=(args.f).upper() 
+    kwargs={'Teff':args.t,
+            'z':args.z,
+            'logg':args.g,
+            'radius' :args.R,
+            'distance':args.dist,
+            'vega':args.vega,
+            'exptime':args.exptime,
+            'clobber':args.clobber,
+            'nonoise':args.nonoise
+    }
 
 
-##############################
-# ACTUAL CONVERSION OF PSF:
-##############################
-if args.run != 'all':  
-    kwargs['run']=int(args.run)
-    convert(in_path,out_path,instr,filt,**kwargs)
-else:
-    for i in xrange(1,nruns+1):
-        kwargs['run']=i
+    #-----------------------------------#
+    # SET UP PATHS BASED ON ARGUMENTS:
+    #-----------------------------------#
+    path    = "/Users/lajoie/Documents/Work/Projects/JWST/Simulations/Coronagraphs/MIRI/Dither-LOCI/Results/"
+    in_path = path+instr+'/'+args.f.upper()+"/DitherStep"+args.step+"/Jitter"+args.jitter+"/Originals"
+    out_path= path+instr+'/'+args.f.upper()+"/DitherStep"+args.step+"/Jitter"+args.jitter+"/"
+
+    if   args.vega==False: out_path+='T'+str(args.t)+'_logg'+str(args.g)+'_z'+str(args.z)+'_R'+str(args.R)+'_D'+str(args.dist)+'_t'+str(args.exptime)
+    elif args.vega==True : out_path+='Vega'+'_t'+str(args.exptime)
+
+    if args.nonoise==True: out_path+='_NoNoise'
+
+    if args.rms!=400: 
+        in_path += str(args.rms)+'nm/' #-Grid25pts/'
+        out_path+= '_'+str(args.rms)+'nm'
+    else:
+        in_path +='/'
+        out_path+='/'
+
+    print ' \nInput original PSFs: ',in_path.split("Dither-LOCI")[1]
+    if os.path.isdir(in_path)==False: 
+        print "   --> Input directory does not exist."
+        sys.exit()
+
+    if os.path.exists(out_path): 
+        print '\nOutput folder',out_path.split("Dither-LOCI")[1],'already exists\n'
+    else: 
+        os.mkdir(out_path)
+        print '\nCreating folder',out_path.split("Dither-LOCI")[1]+'\n'
+
+
+    #-----------------------------------#
+    # CALCULATE NRUNS IN DIRECTORY
+    #-----------------------------------#
+    infiles= glob.glob(in_path+"*ScienceTarget*")
+    nruns  = len(infiles)
+
+
+    #-----------------------------------#
+    # CONVERSION OF PSFs:
+    #-----------------------------------#
+    if args.run != 'all':  
+        kwargs['run']=int(args.run)
         convert(in_path,out_path,instr,filt,**kwargs)
+    else:
+        for i in xrange(1,nruns+1):
+            kwargs['run']=i
+            convert(in_path,out_path,instr,filt,**kwargs)
 
 
